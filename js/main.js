6a9491677afd4e414a74eb4ebdf24b42d22a3178
simplyjess04 <jessica_ruiz04@hotmail.com>
2016-12-01 14:26:22 -0500

    updated main js

--- js/main.js
+++ js/main.js
@@ -4,10 +4,7 @@
 	"use strict";
 	console.log("SEAF Fired");
 	
-	//varibales
-	
-	//functions
-	
-	//listeners
-	
+	var height = 525;
+	var width = 890;
+	var margin= {top:30, right:20, bottom: 30, left:50};
 })();
