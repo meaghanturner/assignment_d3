384c8b7e7822db9c3a5173c571b214f06aa709e4
simplyjess04 <jessica_ruiz04@hotmail.com>
2016-12-02 18:47:17 -0500

    updated file

--- data.php
+++ data.php
@@ -1,30 +1,19 @@
 <?php
-
 $username = "root";
 $password = "root";
 $host = "localhost";
 $database = "whaledata";
 
-$server = mysql_connect($host, $username, $password);
-$connection = mysql_select_db($database, $server);
+$link=mysqli_connect($host, $username, $password, $database);
+ 
+$query = "SELECT * FROM tbl_whales";
+$result = mysqli_query($link, $query);
+$rows = array();
+while($r = mysqli_fetch_assoc($result)) {
+    $rows[] = $r;
+}
+echo json_encode($rows);
 
-    $myquery = "
-SELECT  `type`, `years`, `thumbs` FROM  `whales`
-";
-    $query = mysql_query($myquery);
-    
-    if ( ! $query ) {
-        echo mysql_error();
-        die;
-    }
-    
-    $data = array();
 
-    for ($x = 0; $x < mysql_num_rows($query); $x++) {
-        $data[] = mysql_fetch_assoc($query);
-    }
-    
-    echo json_encode($data);     
-     
-    mysql_close($server);
+
 ?>
--- js/main.js
+++ js/main.js
@@ -3,27 +3,27 @@
 (function() {
 	"use strict";
 	console.log("SEAF Fired");
-	
+
+	 var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     var data = this.responseText;
+     // console.log(data);
+     dataa(data);
+    }
+  };
+  xhttp.open("GET", "data.php", true);
+  xhttp.send();
+
+function dataa(data) {
+	console.log(data);
 
 	
-	var xAxis= d3.axisBottom()
-	.scale(xScale)
-	
-//y line
-	var yValue = function(d) {return d["Years"]};
-
-	var yScale=d3.scaleLinear()
-	.range([0, height]);
-
-	
-	var yAxis=d3.axisLeft()
-	.scale(yScale)
-	.tickFormat(formatPercent);
+var height = 525;
+var width = 700;
+var margin= {top:30, right:20, bottom: 30, left:50};
 
 
-
-
-	
 	d3.select('#scatterPlot')
 	.append('svg')
 	.attr('width', width + margin.left + margin.right)
@@ -32,10 +32,8 @@
 	.append("g")
 	.attr("transform", "translate("+margin.left+","+margin.top+")")
 
+}
 
-d3.json("whaledata.php", function (error data) {
-	data.forEach(function(d){
-		
-	})
-})
+
+
 })();
