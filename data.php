b6f01e567b0f5743772da0c92ca3e24d21def2f5
simplyjess04 <jessica_ruiz04@hotmail.com>
2016-12-02 20:18:35 -0500

    Update data.php

--- data.php
+++ data.php
@@ -1,14 +1,10 @@
 <?php
-<<<<<<< HEAD
-=======
 
->>>>>>> master
 $username = "root";
 $password = "root";
 $host = "localhost";
 $database = "whaledata";
 
-<<<<<<< HEAD
 $link=mysqli_connect($host, $username, $password, $database);
  
 $query = "SELECT * FROM tbl_whales";
@@ -19,30 +15,4 @@ while($r = mysqli_fetch_assoc($result)) {
 }
 echo json_encode($rows);
 
-
-
-=======
-$server = mysql_connect($host, $username, $password);
-$connection = mysql_select_db($database, $server);
-
-    $myquery = "
-SELECT  `type`, `years`, `thumbs` FROM  `whales`
-";
-    $query = mysql_query($myquery);
-    
-    if ( ! $query ) {
-        echo mysql_error();
-        die;
-    }
-    
-    $data = array();
-
-    for ($x = 0; $x < mysql_num_rows($query); $x++) {
-        $data[] = mysql_fetch_assoc($query);
-    }
-    
-    echo json_encode($data);     
-     
-    mysql_close($server);
->>>>>>> master
 ?>
