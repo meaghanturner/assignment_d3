7085943ad42f01cac04773e25e1ac38e41720afb
mturnerdesigns <meaghan.turner9@gmail.com>
2016-12-05 16:31:52 -0500

    the graph
    finally showing graph via DB and adding the scatter plots

--- js/main.js
+++ js/main.js
@@ -1,62 +1,63 @@
 // / JavaScript Document
 
-// (function() {
-// 	"use strict";
-// 	console.log("SEAF Fired");
+(function() {
+	"use strict";
+	console.log("SEAF Fired");
 
-// 	 var xhttp = new XMLHttpRequest();
-//   xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//      var data = this.responseText;
-//      // console.log(data);
-//      dataa(data);
+	 var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     var data = this.responseText; //need to output it as a JSON file not as a string
+     var data = JSON.parse(data);
+     console.log(data);
+     // for (var i = data.length - 1; i >= 0; i--) {
+     // 	console.log(data[i])
+     // 	data[i];
+     // };
+     dataa(data);
 
-//     }
-//   };
-//   xhttp.open("GET", "data.php", true);
-//   xhttp.send();
-
-
-// function dataa(data) {
-// 	console.log(data);
-
-// var height = 525;
-// var width = 700;
-// var margin= {top:30, right:20, bottom: 30, left:50};
+    }
+  };
+  xhttp.open("GET", "data.php", true);
+  xhttp.send();
 
 
+function dataa(data) {
+	console.log(data);
 
-	// d3.select('#scatterPlot')
-	// .append('svg')
-	// .attr('width', width + margin.left + margin.right)
-	// .attr('height',height + margin.top + margin.bottom)
-	// .style('background', '#efefef')
-	// .append("g")
-	// .attr("transform", "translate("+margin.left+","+margin.top+")")
+var w = 900; //"100%";
+var h = 600;
 
-// }
-var data = [1, 2, 3, 4, 5];
+var x = d3.scaleLinear()
+.domain([0, d3.max(data)]) //start and end point of data 
+.range ([0, w]);
 
-var scale = d3.scale.linear()
-	.domain([1,5])
-	.range([0, 200]);
+//Create SVG 
+var svg = d3.select('#scatterPlot')
+	.append('svg')
+	.attr('width', w)
+	.attr('height', h)
+	.style('background', "#efefef");
 
-var svg = d3.select("body")
-	.append("svg")
-	.attr(width, height);
+svg.selectAll('circle')
+	.data(data)
+	.enter()
+	.append('circle')
+	.attr("cx",  function(d) {
+    //return Math.sqrt(h - d[1]);
+    return d.whales_type;
+  })
+  	.attr("cy",  function(d) {
+    //return Math.sqrt(h - d[1]);
+    return d.whales_years;
+  })
+  	.attr("r", function(d) {
+  	//return Math.sqrt(h - d[1]);
+  	return d.whales_years/3;
+	})
+	.style("fill", "#00aa88");
 
-var rects = svg.selectAll("rect").data(data);
-	rects.enter()
-		.append("rect")
-		.attr("x", scale)
-		.attr("y", 50)
-		.attr("width", 50)
-		.attr("height", 50)
+}
 
-	.style("fill": "blue");
 
-//removing / thow away 
-	rects.exit().remove();
-})
 })();
-
